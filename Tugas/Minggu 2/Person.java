e23866c32134968e38fec97f76407add00f03948
Adilah Qurrotu'aini <adilah.qurrotu.aini@gmail.com>
2021-03-16 14:01:00 +0700

    Update Person.java

--- Tugas/Minggu 2/Person.java
+++ Tugas/Minggu 2/Person.java
@@ -1,45 +1,28 @@
 package com.adilah.week2;
 
-class Person {
-    private final String fName;
-    private final String lName;
-    private final String stuId;
-    private boolean stuStatus;
+class Prson {
+    String fName;
+    String lName;
+    String stuId;
+    boolean stuStatus;
 
-    public Person(String fName, String lName, String stuId, boolean stuStatus) {
+    public void set(String fName, String lName, String stuId, boolean stuStatus) {
         this.fName = fName;
         this.lName = lName;
         this.stuId = stuId;
         this.stuStatus = stuStatus;
     }
-
-    public void identifyStudent() {
-        System.out.printf("Name\t\t\t: %s %s\n", fName, lName);
-        System.out.println("Student ID\t\t: " + stuId);
-        System.out.println("Student Status\t: " + (stuStatus ? "Active" : "Inactive"));
+    public void show() {
+        System.out.println("Student Indetify");
+        System.out.println("Student Name\t: "+fName+lName);
+        System.out.println("Student ID\t\t: "+stuId);
+        System.out.println("Student Status\t: "+(stuStatus ? "Active" : "Inactive"));
     }
-    public String getfName() {
-        return fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public String getStuId() {
-        return stuId;
-    }
-
-    public boolean getStuStatus() {
-        return stuStatus;
-    }
-
-    public void setStuStatus(boolean stuStatus) {
-        this.stuStatus = stuStatus;
-    }
-
+}
+public class Person {
     public static void main(String[] args) {
-        Person student = new Person("Lisa", "Palombo", "123", true);
-        student.identifyStudent();
+        Prson student = new Prson();
+        student.set("Lisa", "Palombo", "123456789", true);
+        student.show();
     }
 }
