c686c5107a7de7e1a0e3c11fb9a5c00d0e28fc17
Adilah Qurrotu'aini <adilah.qurrotu.aini@gmail.com>
2021-03-30 19:49:39 +0700

    Latihan Minggu 4

---
+++ Latihan/Minggu 4/ForEach.java
@@ -0,0 +1,13 @@
+package com.adilah.lat4;
+
+public class ForEach {
+    public static void main(String[] args) {
+        // membuat array
+        int angka[] = {3, 1, 42, 24, 12};
+
+        // menggunakan perulangan For each untuk menampilkan angka
+        for (int x : angka) {
+            System.out.print(x + " ");
+        }
+    }
+}
---
+++ Latihan/Minggu 4/While.java
@@ -0,0 +1,14 @@
+package com.adilah.lat4;
+
+public class While {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i <= 10) {
+            //blok kode yang akan diulangi
+            System.out.println("Perulangan ke-" + i);
+            //increment nilai i
+            i++;
+        }
+    }
+}
